f2f8dc55f0b458ca96dd57d19f5fa9b04c26b26c
rittu <145543252+ritzbizkit@users.noreply.github.com>
2025-07-24 23:25:04 +0800

    link change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Any request starting with /api will be sent to the target server
       '/api': {
-        target: 'https://hikkinomore-buddy-server.onrender.com', // The REAL server URL
+        target: 'https://interpause.github.io/hikkinomore-buddy-server', // The REAL server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // This removes /api before sending
       },
